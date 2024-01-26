ba3f6db091c4e49c3fb1e7a3b19c01282c107277
Diego Bouchet <diegobouchet88@gmail.com>
2024-01-26 12:10:44 +0100

    add floats

--- pycpack/mymodule.c
+++ pycpack/mymodule.c
@@ -5,6 +5,10 @@ int add(int a, int b) {
     return a + b;
 }
 
+double add_f(double a, double b){
+    return a + b;
+}
+
 static PyObject *py_add(PyObject *self, PyObject *args) {
     int a, b;
     if (!PyArg_ParseTuple(args, "ii", &a, &b)) {
@@ -13,9 +17,18 @@ static PyObject *py_add(PyObject *self, PyObject *args) {
     return PyLong_FromLong(add(a, b));
 }
 
+static PyObject *py_add_f(PyObject *self, PyObject *args){
+    double a, b;
+    if (!PyArg_ParseTuple(args, "dd", &a, &b)) {
+        return NULL;
+    }
+    return PyFloat_FromDouble(add_f(a, b));
+}
+
 
 static PyMethodDef methods[] = {
     {"add", py_add, METH_VARARGS, "Add two integers."},
+    {"add_f", py_add_f, METH_VARARGS, "Add two floats."},
     {NULL, NULL, 0, NULL}  // Sentinel
 };
 
